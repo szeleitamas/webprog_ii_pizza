3f593d7f3087fc4a2b7eee2a3953c53d16e208ab
szeleitamas <90031868+szeleitamas@users.noreply.github.com>
2022-10-11 18:55:55 +0200

    dbh.class

---
+++ dbh.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class Dbh {
+    private $hostName = "szelei.hu";
+    private $dbName = "szeleihu_pizza";
+    private $userName = "szeleihu_pizza";
+    private $password = "pY67ZdFQNQnZ";
+
+    protected function connect() {
+        try {
+            $dsn = 'mysql:host=' .$this->hostName .';dbname=' .$this->dbName;
+            $pdo = new PDO($dsn, $this->userName, $this->password);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        }
+        catch (PDOException $exception) {
+            echo "Nincs adatbáziskapcsolat!" .$exception->getMessage();
+        }
+    }
+}
